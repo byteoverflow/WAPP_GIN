73bc39e02879d24b24e0ca06beac7e21839a4912
Marko Stojanoski <m.stojanoski@mail.com>
2015-01-31 16:36:29 +0100

    Changed popup text.

--- application/views/v_main.php
+++ application/views/v_main.php
@@ -112,11 +112,13 @@
 		  padding:20px;
 		}
 		#boxes #dialog {
-		  width: 1100px; 
-		  height:600px;
+		  width: 1200px; 
+		  height:632px;
 		  padding:10px;
-		  color:#FFFFFF;
-		  background-color:#007294;
+		  color: #000000;
+		  background-color:#FFFFFF;
+		  font-family: "Century Gothic";
+		  font-size: 16px;
 		}
 		</style>
 	</head>
@@ -124,24 +126,29 @@
 		<div id="boxes">
 			<div style="top: 199.5px; left: 551.5px; display: none;" id="dialog" class="window">			
 			
-			<b>Mirës e erdhët në Klubin  UNË JAM FUQIA – I Am Power</b> <br><br>
-			Në fillim dua që të shënoni dikund këtë datë, dhe momentin që kur u futët në këtë klub, pasi që nga ky moment jeta Juaj do të merr një kahje tjetër, pozitive dhe përplot suksese.<br>
-			Suksesi fillon në momentin që i themi vetvetes që jemi të gatshëm të punojmë në maksimum që të arrijmë deri tek kënaqësitë e dëshiruara. Klubi të cilin Ju tani vizitoni dhe të cilit shum shpejt do t’i bashkangjiteni, nuk ofron një tregim ngushëllues me përshtypje mesatare, por është një frymëzim i vërtetë për të gjithë ata të cilët kanë nevoj për inspirim dhe motivim.<br><br>			
+			<b>Mirë se erdhët në Klubin  UNË JAM FUQIA – I Am Power</b> <br><br>
+			Suksesi fillon në momentin që i themi vetvetes që jemi të gatshëm të punojmë në maksimum që të arrijmë deri tek kënaqësitë e dëshiruara. Klubi të cilin Ju tani vizitoni dhe të cilit shum shpejt do t’i bashkangjiteni, nuk ofron një tregim ngushëllues me përshtypje mesatare, por është një frymëzim i vërtetë për të gjithë ata të cilët kanë nevoj për inspirim dhe motivim. 
+			<br><br>
 			<b>UNË JAM FUQIA</b> – nuk Ju premton resurse financiare të pabaza, nuk Ju premton arritje të shpejta marramendase, nuk Ju premton një jetë të lehtë; por Ju premton një sfidë të vërtet. Ju premton që pas kalimit në filtrat e këtij klubi Ju do të keni aftësinë që VET të arrijni në të gjitha paratë  e dëshiruara, që Ju vet të krijoni sukseset Tuaja të një pas njëshme. 
-			Të keni kontrollë absolute mbi mundësitë dhe dëshirat Tuaja të cilat janë pa fund. Klubi Unë Jam Fuqia për Ju do të jetë burim i informative, këshillave dhe mundësive të cilat do të ndikojnë që Ju një herë e përgjithmonë t’I vëndoni temelet e një jete më të mirë. Gjatë kësaj platforme do të ketë momente kur Ju do të dyshoni në aftësitë Tuaja për vazhdim të mëtutjeshëm, por Jo, ato janë momentet kur Ju duhet të jepni sulmin e fundit drejt suksesit. 
-			Të gjithë at të cilët kane arritur në suksesin lakmues, i cili ka zgjatur gjenerata në gjenerata, kanë arritur me përkushtim, vëmendje, këshillim me të tjerët të cilët gjithashtu kanë qenë të suksesshëm, dhe me punë të palodhshme.<br><br>
-			Programa e klubit përbëhet nga një platformë e larmishme e mbushur me një numër të madh të trajnimeve, seminareve, puntorive dhe seansave një në një me biznismen të sukseshëm. 
-			Qëllimi i kësaj platforme është ndikimi pozitiv në zhvillimin e secilit nga anëtarët, udhëheqja në planet e suksesit të cilat secili nga ne dikund thell në vete i ka të krijuara. 
-			Përgjegjësia për një gjë të tillë nuk është pa bazë, krijuesi i kësaj platforme, njëkohësisht edhe trajnere kryesore është – Mimoza Thaqi – nje grua në biznes e cila ndër vite ka dëshmuar aftësinë e saj për zhvillim të investimeve të suksesshme, që në portfolion e saj ka një numër të konsiderueshëm biznesesh. 
-			Mimoza Thaqi e krijoi këtë mundësi për secilin nga Ju, mundësi që nuk vjen shpeshm mundësi që duhet shfrytzuar në maksimum.<br><br>
-			Përpos që e vëndon vetveten në shërbim të secilit që është i interesuar në zhvillimin e ideve, në platformën Unë Jam Fuqia ajo ka siguruar edhe një numër të madh të folësve të tjerë, biznismenëve të suksseshëm të cilët do të Ju bashkangjiten në rrugëtimin Tuaj dhe do të ndajnë me Ju eksperiencat e tyre dhe njohuritë. 
-			Rrugëtimi në këtë platformë, pas shum puntorive, seminareve dhe trajnimeve do të përfundojë me Certifikatë, të cilën do t’a merr secili i cili I ka kaluar të gjitha nivelete e programës.<br><br>
-			Kjo Certifikatë si dokument do t’ju shërbejë të gjithë atyre të cilët duan të tregojnë njohuritë e tyre në lëmi të ndryshme të zhvillimit profesional, në krijimin e biznesit të ri apo zhvillimin e biznesit egzistues, në nivel më të lartë.<br><br>
-			<b>Momenti definues i suksesit është momenti kur vendosim mos të dorëzohemi!</b><br><br>
-			T’a shtym deri në fund vizionin tonë. Kur gjendemi para sfidave të jetës, ndërsa jemi nisur në rrugëtimin drejt krijimit të një të ardhme më të mirë, me punë të palodhshme, dinjitet, moralitet dhe përkushtim maksimal; sfidat s’janë asgjë tjetër por momente argëtuese, të cilat nuk do të na lejn të n’a mer gjumi e bezdia, por gjithnjë do të n’a ngrisin gjakun e presionin, që t’a përjetojmë rrugëtimin me të gjitha komponentat e gëzimit dhe dështimit.<br>
-			Uroj që së shpejti t’i bashkangjiteni karavanit të suksesit të cilin klubi Unë Jam Fuqia është duke krijuar. Ndryshimet fillojnë me hapa të vogla, rrugëtimi është sfidues, por fundi gjithmon arsyeton mundin. Hapi i parë i Juaji le të jetë anëtarësimi në këtë klub, e më pas vet do të Jeni të aftë të vërejni që Klubi Unë Jam Fuqia është invetsimi më i mirë që Ju e bëtë për biznesin tuaj.     
-			<a href="#" class="close">Close</a> 
-			
+			Të keni kontrollë absolute mbi mundësitë dhe dëshirat Tuaja të cilat janë pa fund. Klubi <b>Unë Jam Fuqia</b> për Ju do të jetë burim i informative, këshillave dhe mundësive të cilat do të ndikojnë që Ju një herë e përgjithmonë t’I vëndoni temelet e një jete më të mirë. Gjatë kësaj platforme do të ketë momente kur Ju do të dyshoni në aftësitë Tuaja për vazhdim të mëtutjeshëm, por Jo, ato janë momentet kur Ju duhet të jepni sulmin e fundit drejt suksesit. 
+			Të gjithë at të cilët kane arritur në suksesin lakmues, i cili ka zgjatur gjenerata në gjenerata, kanë arritur me përkushtim, vëmendje, këshillim me të tjerët të cilët gjithashtu kanë qenë të suksesshëm, dhe me punë të palodhshme.
+			<br><br>
+			 Programa e klubit përbëhet nga një platformë e larmishme e mbushur me një numër të madh të trajnimeve, seminareve, puntorive dhe seansave një në një me biznismen të sukseshëm. Qëllimi i kësaj platforme është ndikimi pozitiv në zhvillimin e secilit nga anëtarët, udhëheqja në planet e suksesit të cilat secili nga ne dikund thell në vete i ka të krijuara. 
+			 Përgjegjësia për një gjë të tillë nuk është pa bazë, krijuesi i kësaj platforme, njëkohësisht edhe trajnere kryesore është – Mimoza Thaqi – nje grua në biznes e cila ndër vite ka dëshmuar aftësinë e saj për zhvillim të investimeve të suksesshme, që në portfolion e saj ka një numër të konsiderueshëm biznesesh. 
+			 Mimoza Thaqi e krijoi këtë mundësi për secilin nga Ju, mundësi që nuk vjen shpeshm mundësi që duhet shfrytzuar në maksimum.
+			 <br><br>
+			 Përpos që e vëndon vetveten në shërbim të secilit që është i interesuar në zhvillimin e ideve, në platformën <b>Unë Jam Fuqia</b> ajo ka siguruar edhe një numër të madh të folësve të tjerë, biznismenëve të suksseshëm të cilët do të Ju bashkangjiten në rrugëtimin Tuaj dhe do të ndajnë me Ju eksperiencat e tyre dhe njohuritë. Rrugëtimi në këtë platformë, pas shum puntorive, seminareve dhe trajnimeve do të përfundojë me Certifikatë, të cilën do t’a merr secili i cili I ka kaluar të gjitha nivelete e programës.
+			 <br><br>
+			 Kjo Certifikatë si dokument do t’ju shërbejë të gjithë atyre të cilët duan të tregojnë njohuritë e tyre në lëmi të ndryshme të zhvillimit profesional, në krijimin e biznesit të ri apo zhvillimin e biznesit egzistues, në nivel më të lartë. 
+			 <br><br>
+			 <b>Momenti definues i suksesit është momenti kur vendosim mos të dorëzohemi! </b>
+			 <br><br>
+			 T’a shtym deri në fund vizionin tonë. Kur gjendemi para sfidave të jetës, ndërsa jemi nisur në rrugëtimin drejt krijimit të një të ardhme më të mirë, me punë të palodhshme, dinjitet, moralitet dhe përkushtim maksimal; sfidat s’janë asgjë tjetër por momente argëtuese, të cilat nuk do të na lejn të n’a mer gjumi e bezdia, por gjithnjë do të n’a ngrisin gjakun e presionin, që t’a përjetojmë rrugëtimin me të gjitha komponentat e gëzimit dhe dështimit.
+			 <br><br>
+			 Uroj që së shpejti t’i bashkangjiteni karavanit të suksesit të cilin klubi <b>Unë Jam Fuqia</b> është duke krijuar. Ndryshimet fillojnë me hapa të vogla, rrugëtimi është sfidues, por fundi gjithmon arsyeton mundin. Hapi i parë i Juaji le të jetë anëtarësimi në këtë klub, e më pas vet do të Jeni të aftë të vërejni që <b>Klubi Unë Jam Fuqia</b> është invetsimi më i mirë që Ju e bëtë për biznesin tuaj.
+			 <a href="#" class="close">Close</a> 
+			 <br>
+			 <img src="<?php echo base_url('assets/css/images/signature.png'); ?>" alt="Image not found"/>
 		</div>
 		<!-- Mask to cover the whole screen -->
 		<div style="width: 1478px; height: 602px; display: none; opacity: 0.8;" id="mask"></div>
